8fdb8ea36d308908a0292916a21eb23bdbb1a0fe
demon_lin <zhong130130130@163.com>
2021-12-25 10:20:13 +0800

    refactor: 更新虎牙弹幕渲染规则

--- huyaLiveChatFontColor.js
+++ huyaLiveChatFontColor.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         虎牙公屏去字体颜色
 // @namespace    https://github.com/demon-zhonglin/tmJavaScript
-// @version      0.1
+// @version      0.2
 // @description  虎牙去公屏文字颜色(改为默认:黑)
 // @author       demon-zhonglin
 // @include      https://www.huya.com/*
@@ -14,13 +14,30 @@
 
   const _style = document.createElement('style')
   _style.type = 'text/css'
-  const styleText = document.createTextNode(".change_color { color: #000 !important; } .change_color li span.msg { color: #000 !important; }")
+  const styleText = document.createTextNode(".chat_item_change_color { color: #000 !important; } .chat_item_change_color div div, .chat_item_change_color div div div span.msg { color: #000 !important; }")
   _style.appendChild(styleText)
   document.body.appendChild(_style)
 
-  const roomMsgList = window.document.querySelector('ul#chat-room__list')
-  const msgListClassName = roomMsgList.className || ''
-  if (!msgListClassName.includes('change_color')) {
-    roomMsgList.className = `${msgListClassName} change_color`
+  function handlerChangeColor() {
+    if (!(document.getElementById('chat-room__wrap').className || '').includes('chat_item_change_color')) {
+      if (document.getElementById("chat-room__wrap").classList) { // 支持 classList 环境
+        document.getElementById('chat-room__wrap').classList.add('chat_item_change_color')
+      } else {
+        document.getElementById('chat-room__wrap').className = `${document.querySelector('chat-room__wrap').className} chat_item_change_color`
+      }
+    }
   }
+
+  const waitTimeNum = 20 // 单位:秒
+  let timeNum = 0 // 单位:秒
+
+  const timer = window.setInterval(() => {
+    timeNum += 1
+    if (timeNum >= waitTimeNum) window.clearInterval(timer)
+
+    if (document.getElementById('chat-room__wrap')) {
+      window.clearInterval(timer)
+      handlerChangeColor()
+    }
+  }, 1000);
 })();
