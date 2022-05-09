2928fdb98dc9f552a41147f1ae77593fc37a00fc
demon_lin <zhong130130130@163.com>
2022-05-10 00:36:11 +0800

    doc: update description

--- biliLiveChangBgImage.js
+++ biliLiveChangBgImage.js
@@ -2,7 +2,7 @@
 // @name         b站直播间去背景图
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  b站直播间去背景图
+// @description  b站直播间去直播框上下背景图
 // @author       demon-zhonglin
 // @match        https://live.bilibili.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
