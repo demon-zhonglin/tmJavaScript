eda28fc2fb569e49ad6b23d76f119d545bb4b47a
demon_lin <zhong130130130@163.com>
2022-05-14 00:31:30 +0800

    refactor: 方式失效,注释掉

--- huyaNoLoginVideoQuality.js
+++ huyaNoLoginVideoQuality.js
@@ -2,7 +2,7 @@
 // @name         虎牙免登陆清晰度
 // @namespace    https://github.com/demon-zhonglin/tmJavaScript
 // @version      0.3
-// @description  虎牙免登陆可控清晰度
+// @description  虎牙免登陆可控清晰度(2022-05-12:虎牙更新后失效了)
 // @author       demon-zhonglin
 // @include      https://*.huya.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -12,6 +12,8 @@
 (function() {
   'use strict';
 
+  /*
+  目前失效
   function getCookieValueByName(name) { // 从 cookie 中获取 key 值
     var match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
     return match ? match[2] : "";
@@ -31,5 +33,6 @@
       })
     }
   }
+  */
 
 })();
