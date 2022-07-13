59ca9401030f5d1bb355d63ada90aabe30171510
demon_lin <zhong130130130@163.com>
2022-07-14 01:16:30 +0800

    fix: 为判断登录状态 #登录下直接改为黑暗模式；非登录下判断是否黑暗模式再改背景图

--- biliLiveChangBgImage.js
+++ biliLiveChangBgImage.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         免登录 b站直播间 深色模式
 // @namespace    https://github.com/demon-zhonglin/tmJavaScript
-// @version      0.5
+// @version      0.6
 // @description  b站直播间去直播框上下背景图(先启用b站默认深色模式,在修改背景图)
 // @author       demon-zhonglin
 // @match        https://live.bilibili.com/*
@@ -12,13 +12,15 @@
 (function() {
   'use strict';
 
+  
   const nodeIDList = ['#head-info-vm', '#gift-control-vm', '#rank-list-vm', '#rank-list-ctnr-box', '#chat-control-panel-vm', '.room-bg.webp.p-fixed']
   const roomBgNode = ".room-bg.webp.p-fixed"
 
-  const log = (data = {}, type = 'table') => {
-    console.log('======= tampermonkey ========')
-    console[type](data)
-    console.log('======= tampermonkey ========')
+  const getCookie = (name) => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+    return ''
   }
 
   const waitNodeBili = ({ selector = '', timeNum = 500, callback = Function.prototype, frq = 0}) => {
@@ -46,7 +48,7 @@
         selector: item,
         callback: (node, _selector) => {
           // const node = document.querySelector(_selector)
-          console.log('node', node.id, [node])
+          // console.log('node', node.id, [node])
           node.style.setProperty('background-image', "url()", 'important')
           node.style.setProperty('background-color', "#181b1d", 'important')
         }
@@ -76,16 +78,23 @@
     }
   })
 
-  waitNodeBili({
-    selector: '#sidebar-vm .side-bar-cntr',
-    callback: (_node, selector) => {
-      _node.style.cssText = 'height: 120px;'
-      const _selector = `${selector} > div:nth-child(2)`
-      const laboratory = window.document.querySelector(`${_selector}`)
-      laboratory.style.cssText = 'dispaly: block;'
-      showMenu()
+  const isNologin = getCookie('DedeUserID')
+  if (!isNologin) { // 无登录
+    waitNodeBili({
+      selector: '#sidebar-vm .side-bar-cntr',
+      callback: (_node, selector) => {
+        _node.style.cssText = 'height: 120px;'
+        const _selector = `${selector} > div:nth-child(2)`
+        const laboratory = window.document.querySelector(`${_selector}`)
+        laboratory.style.cssText = 'dispaly: block;'
+        showMenu()
+      }
+    })
+  } else {
+    if (getCookie('theme_style') === dark) { // 登录状态是黑暗模式,移除背景图
+      changBg()
     }
-  })
+  }
 
   // 去除 roombg:after 背景图
   try {
@@ -96,6 +105,8 @@
     `
     document.head.appendChild(nodeAfterStyle);
   } catch (error) {
-    log({ error: error })
+    console.log('===== tampermonkey =====')
+    console.log('error', error)
+    console.log('===== tampermonkey =====')
   }
 })();
